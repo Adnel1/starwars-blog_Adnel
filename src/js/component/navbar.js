d77ba840b66f955e8c89d36abe2c5b06b8a7fc62
Jorge-Adnel Martin <Repartomoro@hotmail.com>
2024-04-01 19:51:45 +0000

    fixed the position of the dropdown button

--- src/js/component/navbar.js
+++ src/js/component/navbar.js
@@ -3,22 +3,18 @@ import { Link } from "react-router-dom";
 
 export const Navbar = () => {
 	return (		
-  <nav className="navbar navbar-expand-lg navbar-dark bg-light">
-    <div className="container">
-      <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/6/6c/Star_Wars_Logo.svg/2560px-Star_Wars_Logo.svg.png" alt="" width="100" height="60" />
-      <div className="collapse navbar-collapse" id="navbarSupportedContent">
-        <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-          <li className="nav-item dropdown">
-            <button className="btn btn-primary nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-              Dropdown
-            </button>
-            <ul className="dropdown-menu" aria-labelledby="navbarDropdown">
-              <li><a className="dropdown-item" href="#">Action</a></li>
-              <li><a className="dropdown-item" href="#">Another action</a></li>
-              <li><a className="dropdown-item" href="#">Something else here</a></li>
-            </ul>
-          </li>
-        </ul>
+  <nav className="navbar navbar-light bg-light">
+    <div className="container d-flex justify-content-between">
+      <a className="navbar-brand"><img src="https://1000logos.net/wp-content/uploads/2017/06/Star-Wars-Logo-1.png" alt="" width="100" height="60" /></a>
+      <div class="dropdown">
+        <button class="btn btn-primary dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          Favorites
+        </button>
+        <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+          <a class="dropdown-item" href="#">Action</a>
+          <a class="dropdown-item" href="#">Another action</a>
+          <a class="dropdown-item" href="#">Something else here</a>
+        </div>
       </div>
     </div>
   </nav>
